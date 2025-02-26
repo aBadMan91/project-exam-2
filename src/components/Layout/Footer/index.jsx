ba35a9ddcd259f403eb87446a81c00cff17cd7b9
aBadMan <114421846+aBadMan91@users.noreply.github.com>
2025-02-26 18:13:57 +0100

    Register Page
    - Added Register Page, not fully completed.

--- src/components/Layout/Footer/index.jsx
+++ src/components/Layout/Footer/index.jsx
@@ -2,7 +2,7 @@ import React from "react";
 
 export function Footer() {
   return (
-    <footer className="d-flex justify-content-center align-items-center text-white mt-2">
+    <footer className="d-flex justify-content-center align-items-center text-white mt-2 fixed-bottom">
       <p className="my-3">&copy; 2025 Holidaze</p>
     </footer>
   );
--- src/pages/RegisterPage/index.jsx
+++ src/pages/RegisterPage/index.jsx
@@ -1,1 +1,17 @@
 import React, { useEffect } from "react";
+import { RegisterForm } from "../../components/Form/RegisterForm";
+import Container from "react-bootstrap/Container";
+
+export function RegisterPage() {
+  useEffect(() => {
+    document.title = "Holidaze | Register";
+  }, []);
+
+  return (
+    <Container className="mt-5">
+      <h1>Register</h1>
+      <p>Please register with your “stud.noroff.no” email.</p>
+      <RegisterForm />
+    </Container>
+  );
+}
